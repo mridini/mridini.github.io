8776c1f1ae162a1c9269ba7630c48f9f04562fcb
thippisetty <mridinithippisetty@gmail.com>
2024-12-02 16:37:17 -0600

    Save changes to gatsby-node.js (changed max listeners)

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,7 @@
 
 const path = require('path');
 const _ = require('lodash');
+require('events').EventEmitter.defaultMaxListeners = 20; // or any value higher than 10
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
